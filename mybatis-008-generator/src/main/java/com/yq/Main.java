470f538a95cd2a8a8c50cc2def694e62a2bed66e
yiqiang <1228444930@qq.com>
2023-01-08 17:45:06 +0800

    first commit

--- mybatis-008-generator/src/main/java/com/yq/Main.java
+++ mybatis-008-generator/src/main/java/com/yq/Main.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!1111111111");
+        System.out.println("Hello world!111111111122222");
     }
 
     @Test
